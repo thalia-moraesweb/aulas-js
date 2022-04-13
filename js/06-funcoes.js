1467798401577bbd94b4afd7c55bbd4ea4236e20
thalia-moraesweb <moraesthalia.silva@gmail.com>
2022-04-13 14:18:03 -0300

    js funçoes 06

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -23,20 +23,72 @@ console.log(`Escola: ${escola}`);
 linha();
 console.log(`Ano: ${ano}`);
 
+//VERSÃO 1: sintaxe tradicional
          
-              //parâmetros/argumentos
+/*               //parâmetros/argumentos
 function soma(valor1, valor2 ){
     let total= valor1 + valor2;
     return total;
-}
+}*/
 
-console.log(soma(10, 5) );
+//VERSÃO 2:sintaxe ARROW FUNCTION (FUNÇÃO SETA/FLECHA)
+const soma = (valor1, valor2) => {
+    let total = valor1 + valor2;
+    return total;
+};
+
+//Arrow Function com retorno IMPLÍCITO
+const soma1 = (valor1, valor2) => valor1 + valor2;
+
+console.log( soma(150, 55) );
 //console.log(total); //erro, pois é variavel local
 
-let resultado = soma(100, 50);
+let resultado = soma(45, 50);
 console.log(resultado);
 
-if(resultado <1000){
+if(resultado <100){
     console.log("Abaixo da meta")
 }
 
+
+linha(); 
+
+//Versão 1: tradicional
+/* function dobra(numero){
+    let resultado = numero *2;
+    return resultado;
+} */
+/* 
+function dobra(numero){
+    return numero *2;
+ } */
+
+
+//Versão 2: arrow function
+/* const dobra = (numero) => {
+    let resultado = numero *2;
+    return resultado;
+}; */
+/* 
+const dobra = (numero) => {
+    return numero *2;
+}; */
+
+//retorno IMPLICITO (REMOVE CHAVES E RETURN)
+//OBS: como só temos parametro (numero), pode tirar os ()
+const dobra = numero => numero *2
+
+console.log( dobra(10) );
+console.log( dobra(150) );
+
+linha()
+
+let nota1 = 8.5;
+let nota2+ = 7.4;
+
+const calculaMedia= (n1,n2) => (n1+n2) / 2;
+
+console.log(calculaMedia(nota1, nota2) );
+
+//obs.: se não tem parametros, tambem precisa usar ()
+cosnt linha2 = () => console.log("*****");
